9b530f179bf31b58e7c306d3a1547d0ffe164617
dervinism <martynas.dervinis@gmail.com>
2023-04-27 13:53:02 +0200

    Updated README file

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 [Phy](https://github.com/cortex-lab/phy) plugins to help with spikesorting. It contains the following three plugins:
 - ```n_spikes_view``` used to adjust the number of displayed waveforms and recording channels in the WaveformView window.
 - ```recluster``` plugin providing multiple reclustering actions:
-    - Recluster Local PCAs
-    - Recluster Global PCAs
-    - K means clustering
-    - MahalanobisDist
+    - Recluster PCAs to recluster using klustkwik.
+    - K means clustering to recluster using klustkwik with specific number of new clusters.
+    - MahalanobisDist to split off outlier spikes deviating by specified standard deviations in the principal component space.
 - ```SplitShortISI``` plugin providing a reclustering action to split spikes with the inter-spike interval of $\leqslant$ 1.5 ms.
 
 To install plugins, download the contents of this repository (except ```README``` file) and place them inside ```.phy``` folder inside your Windows user directory (```C:\Users\<your-username>\.phy```). Subsequently, open ```phy_config``` file and edit it by replacing ```<your-username>``` entry with the actual name of your user directory.
 
-Plugins were originally created by  for Phy1. \
+Plugins were originally created by Peter Petersen and Thomas Hainmueller for Phy1. \
 Adapted for Phy2 by Martynas Dervinis.
--- plugins/recluster.py
+++ plugins/recluster.py
@@ -36,7 +36,7 @@ class Recluster(IPlugin):
         #def on_create_cluster_views():
         def on_gui_ready(sender,gui):
             @controller.supervisor.actions.add(shortcut='alt+k')
-            def Recluster_Local_PCAs():
+            def Recluster_PCAs():
                 def write_fet(fet, filepath):
                     with open(filepath, 'w') as fd:
                         #header line: number of features
@@ -104,82 +104,6 @@ class Recluster(IPlugin):
                 cmd +=["-UseDistributional",'0',"-MaxPossibleClusters",'20',"-MinClusters",'20'] #,"-MinClusters",'2',"-MaxClusters",'12'   ,"-MaxClusters",'12',"-MaxClusters",'12'
 
                 # Run KlustaKwik
-                p = Popen(cmd)
-                p.wait()
-                # Read back the clusters
-                spike_clusters = read_clusters(name + '.clu.' + str(shank))
-                controller.supervisor.actions.split(spike_ids, spike_clusters)
-                logger.warn("Reclustering complete!")
-
-            @controller.supervisor.actions.add(shortcut='alt+t')
-            def Recluster_Global_PCAs():
-                def write_fet(fet, filepath):
-                    with open(filepath, 'w') as fd:
-                        #header line: number of features
-                        fd.write('%i\n' % fet.shape[1])
-                        #next lines: one feature vector per line
-                        for x in range(0,fet.shape[0]):
-                            fet[x,:].tofile(fd, sep="\t", format="%i")
-                            fd.write ("\n")
-                        #np.savetxt(fd, fet[0], fmt="%i", delimiter=' ')
-
-                def read_clusters(filename_clu):
-                    clusters = load_text(filename_clu, np.int64)
-                    return process_clusters(clusters)
-                def process_clusters(clusters):
-                    return clusters[1:]
-                def load_text(filepath, dtype, skiprows=0, delimiter=' '):
-                    if not filepath:
-                        raise IOError("The filepath is empty.")
-                    with open(filepath, 'r') as f:
-                        for _ in range(skiprows):
-                            f.readline()
-                        x = pd.read_csv(f, header=None,
-                            sep=delimiter).values.astype(dtype).squeeze()
-                    return x
-                
-                """Relaunch KlustaKwik on selected clusters."""
-                # Selected clusters.
-                cluster_ids = controller.supervisor.selected
-                #spike_ids = controller.selector.select_spikes(cluster_ids)
-                bunchs = controller._amplitude_getter(cluster_ids, name='template', load_all=True)
-                spike_ids = bunchs[0].spike_ids
-                logger.info("Running KlustaKwik on %d spikes.", len(spike_ids))
-                # s = controller.supervisor.clustering.spikes_in_clusters(cluster_ids)
-                channel_ids = controller.model.get_cluster_channels(cluster_ids)
-                data3 = controller.model.get_features(spike_ids, channel_ids)
-                fet2 = np.reshape(data3,(data3.shape[0],data3.shape[1]*data3.shape[2]))
-
-                dtype = np.int64
-                factor = 2.**60
-                #dtype = np.int32
-                #factor = 2.**31
-                factor = factor/np.abs(fet2).max()
-                fet2 = (fet2 * factor).astype(dtype)
-                # logger.warn(str(fet2[0,:]))
-
-                # Run KK2 in a temporary directory to avoid side effects.
-                # n = 10
-                # spike_times = controller.model.spike_times[spike_ids]*controller.model.sample_rate
-
-                #spike_times = convert_dtype(spike_times, np.int32)
-                # times = np.expand_dims(spike_times, axis =1)
-                
-                # fet = 1000*np.concatenate((fet2,times),axis = 1)
-                fet = fet2
-
-                name = 'tempClustering'
-                shank = 3
-                mainfetfile = os.path.join(name + '.fet.' + str(shank))
-                write_fet(fet, mainfetfile)
-                if platform.system() == 'Windows':
-                    program = os.path.join(phy_config_dir(),'klustakwik.exe')
-                else:
-                    program = '~/klustakwik/KlustaKwik'
-                cmd = [program, name, str(shank)]
-                cmd +=["-UseDistributional",'0'] # ,"-MinClusters",'2',"-MaxClusters",'12'
-
-                # Run KlustaKwik.
                 p = Popen(cmd)
                 p.wait()
                 # Read back the clusters
